e4ea1a2a3ed9bf40fa969da68dc60aeee660f120
두일 김 <kimduil337@gmail.com>
2026-04-19 19:56:27 +0900

    Fix merge conflicts and update requirements.txt

--- App.py
+++ App.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 
 # 페이지 기본 설정
@@ -16,22 +15,3 @@ if st.button("클릭하세요"):
     st.success("버튼이 클릭되었습니다! 🎉")
 else:
     st.info("버튼을 눌러보세요.")
-=======
-import streamlit as st
-
-# 페이지 기본 설정
-st.set_page_config(
-    page_title="Market Manager", 
-    page_icon="📊",
-    layout = "wide")
-
-st.title("Market Manager 📈")
-st.header("This is Header")
-st.subheader("This Sub Header")
-
-# 버튼 생성 및 로직
-if st.button("클릭하세요"):
-    st.success("버튼이 클릭되었습니다! 🎉")
-else:
-    st.info("버튼을 눌러보세요.")
->>>>>>> 7f55225c77591c9ac07c9ba6fcb2fcfcaafc20fe
--- pages/1_Docs.py
+++ pages/1_Docs.py
@@ -1,4 +1,9 @@
+import streamlit as st
+
 st.set_page_config(
     page_title="Market Manager", 
     page_icon="📊",
     layout = "wide")
+
+st.title("Docs 📄")
+st.write("이 페이지는 문서 페이지입니다.")
