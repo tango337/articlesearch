9f9572ff6d6804f7574b61905ddfc1ddbf402ce2
두일 김 <kimduil337@gmail.com>
2026-04-19 22:13:32 +0900

    Update App.py and requirements.txt

--- App.py
+++ App.py
@@ -2,16 +2,25 @@ import streamlit as st
 
 # 페이지 기본 설정
 st.set_page_config(
-    page_title="Market Manager", 
-    page_icon="📊",
-    layout = "wide")
+    page_title="Power Packaging Solutiuon Task", 
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
-st.title("Market Manager 📈")
-st.header("This is Header")
-st.subheader("This Sub Header")
+st.title("AI Article Search Manager 📈")
+#st.header("This is Header")
+#st.subheader("This Sub Header")
+st.divider()
+
+# 검색 기간 선택
+st.subheader("검색 기간 선택")
+date = st.pills("",["1주 이내","1개월 이내","3개월 이내","6개월 이내","1년 이내","1년 이상"])
+st.divider()
+
+# 검색 분야 선택
+st.subheader("검색 분야 선택")
+#date = st.pills("",["1주 이내","1개월 이내","3개월 이내","6개월 이내","1년 이내","1년 이상"])
+st.divider()
+
 
 # 버튼 생성 및 로직
-if st.button("클릭하세요"):
-    st.success("버튼이 클릭되었습니다! 🎉")
-else:
-    st.info("버튼을 눌러보세요.")
